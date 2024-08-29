ace57182e40861c65dcee89efbc42d1fd73e053d
Cristian-Vasile Achim <cristian-vasile.achim@csc.fi>
2024-08-29 10:50:31 +0300

    out of bounds

---
+++ B3/L9/exercises/out-of-bonds/ftn/out_of_bounds.f90
@@ -0,0 +1,29 @@
+program out_of_bounds
+  implicit none
+  integer           :: i, n = 1000
+  real              :: s
+  real, allocatable :: a(:)
+  allocate (a(n))
+!$acc data create(a) copyout(s)
+!$acc parallel loop
+  do i = 1, n
+     a(i) = i
+  end do
+!$acc end parallel loop
+
+!$acc parallel loop
+  do i = 1, n
+     a(i) = 2*a(i)
+  end do
+!$acc end parallel loop
+
+  s = 0.
+!$acc parallel loop reduction(+:s)
+  do i = 1, n
+  ! caution out-of-bounds access. `compute-sanitizer` and point out this error
+     s = s + a(i+800)
+  end do
+!$acc end parallel loop
+!$acc end data
+  print *, s
+end program
