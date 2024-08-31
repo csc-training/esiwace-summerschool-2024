6676588a87465412fdee76fcffafea10573260f7
Juha Tonttila <juha.tonttila@csc.fi>
2024-08-31 09:39:41 +0300

    Bug fixes L7 & L8

--- B3/L8/exercise/scripts/allocate_job.sh
+++ B3/L8/exercise/scripts/allocate_job.sh
@@ -1,4 +1,4 @@
-#! bin/sh
+#!/bin/sh
 
 salloc --ntasks=4 --account=bb1153 --partition=shared --qos=esiwace --time=00:10:00 
 #salloc --ntasks=1 --cpus-per-task=4 --account=bb1153 --partition=shared --qos=esiwace --time=00:10:00
