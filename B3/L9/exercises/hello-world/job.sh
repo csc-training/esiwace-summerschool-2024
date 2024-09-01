83bc68a881afe23d6d71ab22e7c4bc9071350fca
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2024-09-01 11:55:37 +0300

    Update job.sh

--- B3/L9/exercises/hello-world/job.sh
+++ B3/L9/exercises/hello-world/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=bb1153
 #SBATCH --ntasks=1
-#SBATCH --time=00:15:00
+#SBATCH --time=00:05:00
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
 #SBATCH --reservation=esiwace_warmworld
@@ -12,5 +12,5 @@ srun ./hello
 #  sbatch job.sh
 
 # or alternatively, run directly from the command line:
-#  srun --account=bb1153 --ntasks=1 --time=00:15:00 --gpus=1 \
+#  srun --account=bb1153 --ntasks=1 --time=00:05:00 --gpus=1 \
 #       --partition=gpu --reservation=esiwace_warmworld ./hello
